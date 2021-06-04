2d5e260ce1c7fbb7b8076319af9ba9a7b1ae0d10
kemeb <kemenesi.balazs@gmail.com>
2021-06-04 15:08:39 +0200

    changed to arrow function

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const body = document.querySelector("body");
 const button = document.querySelector(".button");
 const colors = ["blue", "green", "red", "white", "yellow", "black", "purple"];
 
-button.addEventListener("click", backgroundColor);
-function backgroundColor() {
+const changer = () => {
   const choosenColor = parseInt(Math.random() * colors.length);
   body.style.backgroundColor = colors[choosenColor];
-}
+};
+
+button.addEventListener("click", changer);
