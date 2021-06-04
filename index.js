3913349aaee3372373f563569ca39c90601d56e3
kemeb <kemenesi.balazs@gmail.com>
2021-06-04 16:03:34 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const body = document.querySelector("body");
 const button = document.querySelector(".button");
-const colors = ["blue", "green", "red", "white", "yellow", "black", "purple", "#FF5733", "#00649D"];
+const colors = [
+  "blue",
+  "green",
+  "red",
+  "white",
+  "yellow",
+  "black",
+  "purple",
+  "#FF5733",
+  "#00649D",
+];
 
 const changer = () => {
   const choosenColor = parseInt(Math.random() * colors.length);
-  body.style.backgroundColor = colors[choosenColor];
+  const currentColor = colors[choosenColor];
+  body.style.backgroundColor = currentColor;
+  const p = document.getElementById("colorName");
+  p.innerHTML = currentColor;
+  p.style.color = "black";
+  if (currentColor === "black") {
+    p.style.color = "white";
+  }
 };
 
 button.addEventListener("click", changer);
